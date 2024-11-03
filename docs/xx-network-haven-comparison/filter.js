987fbfadb68cfc73d02340316a70977aa7cc925d
ArmchairAncap <armchairancap@users.noreply.github.com>
2024-11-03 14:10:31 +0800

    Add fork of messenger comparison with xx Network Haven entry

---
+++ docs/xx-network-haven-comparison/filter.js
@@ -0,0 +1,132 @@
+'use strict';
+
+(() => {
+
+  const dce = type => document.createElement(type);
+
+  // state der checkboxen
+  let state = true;
+  const initialState = ['Haven', 'Session', 'SimpleX'];
+  const checkboxes = Array.from(document.querySelectorAll('input'));
+  checkboxes.forEach(checkbox => {
+    const messenger = checkbox.labels[0].textContent;
+    checkbox.checked = initialState.includes(messenger);
+    checkbox.dispatchEvent(new CustomEvent('change', { detail: 'dontpush' }));
+  });
+ 
+
+  const toggle = (row, idx, on) => {
+    row.cells[idx].classList.toggle('hide', !on);
+  };
+
+  window.onpopstate = evt => {
+    if (evt.state && evt.state.filter) {
+      setFilter(evt.state.filter);
+    }
+  };
+
+  const setFilter = filter => {
+    for (let checkbox of document.querySelectorAll('input')) {
+      const messenger = checkbox.labels[0].textContent;
+      const checked = checkbox.checked;
+      if (checked !== filter.includes(messenger)) {
+        checkbox.checked = !checked;
+        checkbox.dispatchEvent(new CustomEvent('change', { detail: 'dontpush' }));
+      }
+    }
+  };
+
+  const onChange = evt => {
+    const checkbox = evt.target;
+    const idx = parseInt(checkbox.dataset.idx, 10);
+    if (isNaN(idx)) {
+      return;
+    }
+    Array.from(document.querySelectorAll('thead tr'))
+      .forEach(r => toggle(r, idx, checkbox.checked));
+
+    Array.from(document.querySelectorAll('tbody tr'))
+      .filter(r => r.cells.length > 2)
+      .forEach(r => toggle(r, idx, checkbox.checked));
+
+    const count = document.querySelectorAll('input:checked').length;
+    Array.from(document.querySelectorAll('tbody td[colspan]'))
+      .forEach(td => td.setAttribute('colspan', count));
+
+    document.querySelector('tfoot td:nth-child(2)').setAttribute('colspan', count - 1);
+
+    if (evt.detail === 'dontpush') {
+      return;
+    }
+    const filter = getFiltered();
+    window.history.pushState({ filter }, 'Messenger-Matrix', `?filter=${filter.join(',')}`);
+  };
+
+  const getFiltered = () => Array.from(document.querySelectorAll('input:checked'))
+    .map(checkbox => checkbox.labels[0].textContent);
+
+  const checkbox = cell => {
+    const label = dce('label');
+    const input = dce('input');
+    input.setAttribute('type', 'checkbox');
+    input.dataset.idx = cell.cellIndex;
+    input.checked = true;    
+    input.addEventListener('change', onChange);
+    label.appendChild(input);
+    label.appendChild(document.createTextNode(cell.textContent.replace(/\(.*\)/, '').trim()));
+    return label;
+  };
+
+  const toggleAll = (evt, fieldset, opts) => {
+    evt.stopPropagation();
+    state = !state;
+    for (const cb of fieldset.elements) {
+      cb.checked = state;
+      cb.dispatchEvent(new CustomEvent('change', { detail: 'dontpush' }));
+    }
+    evt.target.textContent = state
+      ? opts.toggleAllButton.offLabel
+      : opts.toggleAllButton.onLabel;
+  };
+
+  const filterform = (opts) => {
+    const button = dce('button');
+    const form = dce('form');
+    const fieldset = dce('fieldset');
+    const onClick = (fieldset) => {
+      fieldset.classList.toggle('hide');
+    };
+    fieldset.classList.add('hide');
+
+    const toggleAllButton = dce('button');
+    toggleAllButton.setAttribute('type', 'button');
+    toggleAllButton.textContent = opts.toggleAllButton.offLabel;
+    toggleAllButton.addEventListener('click', (evt) => toggleAll(evt, fieldset, opts));
+
+    fieldset.appendChild(toggleAllButton);
+    Array.from(document.querySelector('thead').rows[0].cells)
+      .filter(c => c.cellIndex > 0)
+      .map(c => checkbox(c))
+      .forEach(cb => fieldset.appendChild(cb));
+    button.addEventListener('click', () => onClick(fieldset));
+    button.textContent = opts.buttonLabel;
+    button.setAttribute('type', 'button');
+    form.appendChild(button);
+    form.appendChild(fieldset);
+    return form;
+  };
+
+  const presetFilterFromUrl = () => {
+    const u = new URLSearchParams(window.location.search);
+    if (u.get('filter')) {
+      setFilter(u.get('filter'));
+    }
+  };
+
+  window.addFilter = opts => {
+    document.body.classList.add('filter');
+    opts.formContainer.prepend(filterform(opts));
+    presetFilterFromUrl();
+  };
+
+})();
---
+++ docs/xx-network-haven-comparison/fontsize.js
@@ -0,0 +1,41 @@
+'use strict';
+(() => {
+
+  const dce = type => document.createElement(type);
+
+  const changeFontSize = (dir, opts) => {
+    const style = getComputedStyle(document.documentElement, null);
+    const fontSize = style.getPropertyValue(opts.property);
+    const unit = fontSize.replace(/\d/g, '').trim();
+    let value = parseFloat(fontSize) + dir * opts.step;
+    if (value < opts.min) {
+      value = opts.min;
+    } else if (value > opts.max) {
+      value = opts.max;
+    }
+    const newSize = `${value}${unit}`;
+    const iconSize = Math.min(1, value / opts.default);
+    document.documentElement.style.setProperty(opts.iconProperty, iconSize);
+    document.documentElement.style.setProperty(opts.property, newSize);
+  };
+
+  const buttons = (opts) => {
+    const inc = dce('button');
+    inc.setAttribute('type', 'button');
+    inc.textContent = opts.increaseButtonLabel;
+    inc.addEventListener('click', () => changeFontSize(1, opts));
+    const dec = dce('button');
+    dec.setAttribute('type', 'button');
+    dec.textContent = opts.decreaseButtonLabel;
+    dec.addEventListener('click', () => changeFontSize(-1, opts));
+    const div = dce('div');
+    div.appendChild(inc);
+    div.appendChild(dec);
+    div.className = 'fontsizecontrol';
+    return div;
+  };
+
+  window.addFontSizeButtons = (opts) => {
+    opts.container.prepend(buttons(opts));
+  };
+})();
