a4121e65f46c639139d61d055cfe506636bf99c2
ArmchairAncap <armchairancap@users.noreply.github.com>
2024-11-03 16:04:05 +0800

    Blog post with notes on forked comparison of messengers

---
+++ docs/assets/js/21866543.28595ffd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkaa=self.webpackChunkaa||[]).push([[5482],{4754:a=>{a.exports=JSON.parse('{"tag":{"label":"speakeasy","permalink":"/blog/tags/speakeasy","allTagsPath":"/blog/tags","count":2,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/speakeasy","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ docs/assets/js/404e42f6.ab4f0d4f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkaa=self.webpackChunkaa||[]).push([[7960],{1678:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":16,"previousPage":"/blog","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
